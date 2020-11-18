8c995609f5a252931654becacb4c93ecb3eb29e6
Trevis42 <sirtrevis42@gmail.com>
2020-11-18 15:14:27 -0600

    updated stuff

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -1,8 +1,12 @@
 package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
+import com.zipcodewilmington.tools.RandomNumberFactory;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 /**
  * Created by leon on 5/1/17.
@@ -19,15 +23,35 @@ public final class PhoneNumberFactory {
      * @param phoneNumberCount - number of PhoneNumber objects to instantiate
      * @return array of randomly generated PhoneNumber objects
      */ //TODO - Implement logic
-    public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) {
-        return null;
+    public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) throws InvalidPhoneNumberFormatException {
+        return (PhoneNumber[]) Arrays
+                .stream(new PhoneNumber[]{createRandomPhoneNumber()})
+                .limit(phoneNumberCount)
+                .toArray();
     }
 
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
-    public static PhoneNumber createRandomPhoneNumber() {
-        return createPhoneNumberSafely(-1, -1, -1);
+    public static PhoneNumber createRandomPhoneNumber() throws InvalidPhoneNumberFormatException {
+//        Integer rng = ThreadLocalRandom.current().nextInt(0, 10);
+//        StringBuilder areaCode = new StringBuilder();
+//        StringBuilder centralOfficeCode = new StringBuilder();
+//        StringBuilder phoneLineCode = new StringBuilder();
+//
+//        areaCode.append(rng).append(rng).append(rng);
+//        centralOfficeCode.append(rng).append(rng).append(rng);
+//        phoneLineCode.append(rng).append(rng).append(rng).append(rng);
+
+//        return createPhoneNumberSafely(
+//                Integer.parseInt(String.valueOf(areaCode)),
+//                Integer.parseInt(String.valueOf(centralOfficeCode)),
+//                Integer.parseInt(String.valueOf(phoneLineCode)));
+        //Leon did it this way
+        return createPhoneNumberSafely(
+                RandomNumberFactory.createInteger(100,999),
+                RandomNumberFactory.createInteger(100,999),
+                RandomNumberFactory.createInteger(1000,9999));
     }
 
 
@@ -36,17 +60,27 @@ public final class PhoneNumberFactory {
      * @param centralOfficeCode - 3 digit code
      * @param phoneLineCode     - 4 digit code
      * @return a new phone number object
-     */ //TODO - if input is valid, return respective PhoneNumber object, else return null
+     */
     public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+        StringBuilder phoneNumber = new StringBuilder();
+        phoneNumber.append(String.format("(%s)-", areaCode))
+                .append(String.format("%s-", centralOfficeCode))
+                .append(String.format("%s", phoneLineCode));
+        try {
+            return createPhoneNumber(phoneNumber.toString());
+        } catch (InvalidPhoneNumberFormatException e) {
+            return null;
+        }
     }
 
     /**
      * @param phoneNumberString - some String corresponding to a phone number whose format is `(###)-###-####`
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
-     */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+     */
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException {
+        PhoneNumber phoneNumber;
+        phoneNumber = new PhoneNumber(phoneNumberString);
+        return phoneNumber;
     }
 }
